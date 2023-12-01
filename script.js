5a19ed53c0b6576047060b39de9bbab4d8ec0e96
Rukshona Rad <rukshona.rad@gmail.com>
2023-11-30 21:42:34 -0500

    Add nav for menu

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-// const burgerIcon = document.getElementById("mobile-nav-icon");
-
-// const mobileNav = document.querySelector(".mobile-nav");
-
-// burgerIcon.addEventListener("click", () => {
-//     mobileNav.style.display = "inline-block";
-// });
-
-// const closeIcon = document.querySelector(".close-icon");
-
-// closeIcon.addEventListener("click", () => {
-//     mobileNav.style.display = "none";
-// });
-
 const burgerIcon = document.getElementById("mobile-nav-icon");
 
 const mobileNav = document.querySelector(".mobile-nav");
+mobileNav.style.padding = "4rem 2.8rem";
+mobileNav.style.textAlign = "center";
 
 burgerIcon.addEventListener("click", () => {
     mobileNav.classList.add("show-menu");
@@ -25,41 +13,15 @@ const closeIcon = document.querySelector(".close-icon");
 closeIcon.addEventListener("click", () => {
     mobileNav.classList.remove("show-menu");
 });
-const styles = `
-    .menu-list {
-      list-style: none;
-      padding: 0;
-    }
-    .menu-list li {
-      margin-bottom: 10px;
-    }
-    .menu-list li a {
-      text-decoration: none;
-      color: #333;
-      font-weight: bold;
-      font-size: 18px;
-    }
-    .menu-list li a:hover {
-      color: #ff0000;
-    }
-  `;
-document.addEventListener("DOMContentLoaded", function () {
-    const menuItems = ["Home", "About", "Services", "Contact"];
-    const menuList = document.querySelector(".menu-list");
 
-    menuItems.forEach((itemText) => {
-        const listItem = document.createElement("li");
-        const link = document.createElement("a");
-        link.href = "#";
-        link.textContent = itemText;
-        listItem.appendChild(link);
-        menuList.appendChild(listItem);
-    });
+const logoMobileNav = mobileNav.querySelector(".mobile-nav_link");
+logoMobileNav.style.width = "10rem";
+logoMobileNav.style.marginBottom = "3rem";
 
-    const burgerMenu = document.querySelector(".burger-menu");
-    const menuListContainer = document.querySelector(".navbar");
+const linksInsideMobileNav = mobileNav.querySelectorAll("a");
 
-    burgerMenu.addEventListener("click", function () {
-        menuListContainer.classList.toggle("show-menu");
-    });
+linksInsideMobileNav.forEach((link) => {
+    link.style.fontSize = "5rem";
+    link.style.lineHeight = "4rem";
+    link.style.marginBottom = "15rem";
 });
